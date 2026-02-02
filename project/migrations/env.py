68ffe55a113b5fd795d1b612073689ad3615d1a4
david.ringgenberg <David.Ringgenberg@students.fhnw.ch>
2026-02-02 10:18:53 +0100

    update: docker-compose and configuration to use Docker secrets for sensitive data

--- project/migrations/env.py
+++ project/migrations/env.py
@@ -31,7 +31,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}")
+config.set_main_option("sqlalchemy.url", f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD_FILE=/run/secrets/database_password')}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}")
 
 
 def run_migrations_offline() -> None:
--- project/src/database/connection.py
+++ project/src/database/connection.py
@@ -5,7 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
+db_password = None
+if os.path.exists('/run/secrets/database_password'):
+    with open('/run/secrets/database_password', 'r') as f:
+        db_password = f.read().strip()
+else:
+    db_password = os.getenv('DATABASE_PASSWORD')
+
+DATABASE_URL = f"postgresql://{os.getenv('DATABASE_USER')}:{db_password}@{os.getenv('DATABASE_HOST')}:{os.getenv('DATABASE_PORT')}/{os.getenv('DATABASE_NAME')}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
